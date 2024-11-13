85f55643eb37153444e8d28f4165952eb5bf765d
Harshit <harshitrajput557@gmail.com>
2024-11-13 20:07:36 +0530

    Initial commit of Guess the Movie project

---
+++ src/test/java/com/example/gtmyt/GtmytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtmyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
